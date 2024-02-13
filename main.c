153bfb395c0a90b1999ba9a1acb8acde5ad0702a
danny <d4nny815@gmail.com>
2024-02-13 15:16:15 -0800

    branch

--- main.c
+++ main.c
@@ -6,32 +6,13 @@
 int main(void) {
 	OffFile* off_file = read_off_file("example.off");
 
-	printf("Vertices: %d\n", off_file->details.vertices);
-	printf("Faces: %d\n", off_file->details.faces);
-	printf("Edges: %d\n", off_file->details.edges);
-
-	// for (unsigned int i=0; i<off_file->vertices->index; i++) {
-		// printf("v%d: ", i);
-		// print_vertex((Vertex*)array_list_get(off_file->vertices, i));
-	// }
-	// printf("\n");
-// 
-	// for (unsigned int i=0; i<off_file->faces->index; i++) {
-		// printf("Face%d: ", i);
-		// print_face((Face*)array_list_get(off_file->faces, i));
-	// }
-	// printf("\n");
-
 	const float TRANSFORM_MATRIX[] = {1, 0, 0, 0, 
 									0, 1, 0, 0, 
 									0, 0, 1, 0, 
 									0, 0, 0, 1};
 
-	printf("Before transformation: ");
-	print_vertex((Vertex*)array_list_get(off_file->vertices, 0));
-	transformVertex((Vertex*)array_list_get(off_file->vertices, 0), (float*)TRANSFORM_MATRIX, 4);
-	printf("After transformation: ");
-	print_vertex((Vertex*)array_list_get(off_file->vertices, 0));
+	Vertex* v = (Vertex*) array_list_get(off_file->vertices, 0);
+	transformVertex(v, (float*) TRANSFORM_MATRIX, 4);
 
 	free_off_file(off_file);
 	return 0;
