ab47403da3c90054747ece8fd7c9f97d2d32721c
danny <d4nny815@gmail.com>
2024-02-27 12:53:49 -0800

    done

--- Lighting/Lighting.c
+++ Lighting/Lighting.c
@@ -5,7 +5,7 @@
  * @brief Calculate the intensity of the color based on the normal vector and the light vector
  * @param initial_color The initial color of the face
  * @param normal_vec The normal vector of the face
- * @param light_vec The light vector, assumed to be a unit vector
+ * @param light_vec The light vector, assumed to be a unit vecto
  * @return The new color value
 */
 ColorValue calc_color_intensity(ColorValue initial_color, Vector normal_vec, Vector light_vec) {
